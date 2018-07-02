ff5b00af07698057e4fb50eaf0fec88a71779df3
TanMyBlog <kd.aigapa@gmail.com>
2018-07-02 11:23:12 +0700

    Get list role

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -6,8 +6,8 @@ import { AuthenService } from './authen.service';
 import { SystemConstants } from './../common/system.constants';
 import { Router } from '@angular/router';
 import { Http, Response, Headers } from '@angular/http';
-import { Injectable, RootRenderer } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,31 +15,34 @@ import { Observable, of } from 'rxjs';
 export class DataService {
   private headers: Headers;
   constructor(private _http: Http, private _router: Router, private _authenService: AuthenService,
-    private _notificationService: NotificationService, private _utilityService: UtilityService) { }
+    private _notificationService: NotificationService, private _utilityService: UtilityService) {
+    this.headers = new Headers();
+    this.headers.append('Content-Type', 'application/json');
+  }
 
   get(uri: string) {
     this.headers.delete("Authorization");
-    this.headers.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().access_token);
+    this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
     return this._http.get(SystemConstants.BASE_API + uri, { headers: this.headers }).pipe(map(this.extractData));
   }
   post(uri: string, data?: any) {
     this.headers.delete("Authorization");
-    this.headers.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().access_token);
+    this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
     return this._http.post(SystemConstants.BASE_API + uri, data, { headers: this.headers }).pipe(map(this.extractData));
   }
   put(uri: string, data?: any) {
     this.headers.delete("Authorization");
-    this.headers.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().access_token);
+    this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
     return this._http.put(SystemConstants.BASE_API + uri, data, { headers: this.headers }).pipe(map(this.extractData));
   }
   delete(uri: string, key: string, id: string) {
     this.headers.delete("Authorization");
-    this.headers.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().access_token);
+    this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
     return this._http.delete(SystemConstants.BASE_API + uri + '/?' + key + '=' + id, { headers: this.headers }).pipe(map(this.extractData));
   }
   postFile(uri: string, data?: any) {
     let newHeader = new Headers();
-    newHeader.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().access_token);
+    newHeader.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
     return this._http.post(SystemConstants.BASE_API + uri, data, { headers: this.headers }).pipe(map(this.extractData));
   }
 
@@ -59,9 +62,9 @@ export class DataService {
       let errMsg = (error.message) ? error.message :
         error.status ? `${error.status} - ${error.statusText}` : 'Lỗi hệ thống';
       this._notificationService.printErrorMessage(errMsg);
-
       return Observable.throw(errMsg);
     }
+
   }
 }
 
